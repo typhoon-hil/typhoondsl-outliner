b0d77f3b34de6182812c10bbacf9ff4b0fdc4090
Igor Dejanovic <igor.dejanovic@gmail.com>
2023-05-18 13:44:39 +0200

    feat: improve CLI params handling

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,41 @@ mod outliner_lexer;
 #[cfg(test)]
 mod tests;
 
-use std::{env, path::Path};
-
+use clap::Parser;
 use outliner_lexer::OutlinerLexer;
+use std::path::PathBuf;
 
-fn main() -> ! {
-    match &env::args().collect::<Vec<_>>()[..] {
-        [_, model] => {
-            println!("Parsing: {model}");
-            match outliner::OutlinerParser::new(OutlinerLexer::new()).parse_file(model) {
-                Ok(_) => {
-                    println!("Success!");
-                    std::process::exit(0);
-                }
-                Err(e) => {
-                    println!("Failure!");
-                    println!("{e:#?}");
-                    std::process::exit(1);
-                },
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Should JSON be generated
+    #[arg(short, long)]
+    json: bool,
+
+    #[clap(value_parser, value_name="MODEL", value_hint = clap::ValueHint::FilePath)]
+    model: PathBuf,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match outliner::OutlinerParser::new(OutlinerLexer::new()).parse_file(args.model) {
+        Ok(model) => {
+            if args.json {
+                println!("{}", serde_json::to_string(&model).unwrap());
+            } else {
+                println!("Success!");
             }
+            std::process::exit(0);
         }
-        [exec, ..] => {
-            println!(
-                "Usage: {} <model>",
-                Path::new(exec)
-                    .file_name()
-                    .expect("Unable to get file name.")
-                    .to_string_lossy()
-            );
+        Err(e) => {
+            if args.json {
+                println!(r#"{{ "error": true }}"#);
+            } else {
+                println!("Failure!");
+                println!("{e:#?}");
+            }
             std::process::exit(1);
         }
-        _ => panic!("This should not happen!"),
     }
 }
