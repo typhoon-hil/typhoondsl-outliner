c78eb3eed5a05057337eb7282823c5706d76b3d7
Igor Dejanovic <igor.dejanovic@gmail.com>
2024-02-16 19:16:51 +0100

    reformat lexer code

--- src/outliner_lexer.rs
+++ src/outliner_lexer.rs
@@ -1,11 +1,11 @@
 use crate::outliner::{State, TokenKind};
 
+use crate::outliner_actions::Ctx;
 #[cfg(debug_assertions)]
 use colored::*;
 use lazy_static::lazy_static;
 use regex::Regex;
-use rustemo::{Context, Input, Lexer, Token, Location, Position, log};
-use crate::outliner_actions::Ctx;
+use rustemo::{log, Context, Input, Lexer, Location, Position, Token};
 
 pub type InputType = str;
 
@@ -53,34 +53,24 @@ impl<'i> Lexer<'i, Ctx<'i>, State, TokenKind> for OutlinerLexer {
         i: &'i Self::Input,
         token_kinds: Vec<(TokenKind, bool)>,
     ) -> Box<dyn Iterator<Item = Token<'i, Self::Input, TokenKind>> + 'i> {
-        // 1. Skip WS
         self.skip(c, i);
-        // 2. For each token expect check if it is at the current position
-        //    Remember to take into account flaky matching, i.e. tokens
-        //    which should match only if no other expected tokens are here.
-        //    Flaky tokens should be of low priority.
+
         fn is_word_char(c: char) -> bool {
             c.is_alphabetic() || c == '-' || c == '_'
         }
         fn is_id(s: &str) -> bool {
             s.chars().all(is_word_char)
         }
-        // Match on a word boundaries for ID-like strings
-        fn is_match(s: &str, input: &str,  position: usize) -> bool {
+        /// Match on a word boundaries for ID-like strings
+        fn is_match(s: &str, input: &str, position: usize) -> bool {
             match input.get(position..position + s.len()) {
                 Some(subs) => {
                     subs == s
                         && (!is_id(s)
                             || (!is_word_char(
-                                input[..position]
-                                    .chars()
-                                    .next_back()
-                                    .unwrap_or(' '),
+                                input[..position].chars().next_back().unwrap_or(' '),
                             ) && !is_word_char(
-                                input[position + s.len()..]
-                                    .chars()
-                                    .next()
-                                    .unwrap_or(' '),
+                                input[position + s.len()..].chars().next().unwrap_or(' '),
                             )))
                 }
                 None => false,
@@ -95,7 +85,11 @@ impl<'i> Lexer<'i, Ctx<'i>, State, TokenKind> for OutlinerLexer {
             }
         }
 
-        fn consume_until<'i>(input: &'i str, context: &Ctx, until_words: &[&str]) -> Option<&'i str> {
+        fn consume_until<'i>(
+            input: &'i str,
+            context: &Ctx,
+            until_words: &[&str],
+        ) -> Option<&'i str> {
             let position: usize = input[context.position()..]
                 .char_indices()
                 .take_while(|(idx, _)| {
@@ -113,142 +107,147 @@ impl<'i> Lexer<'i, Ctx<'i>, State, TokenKind> for OutlinerLexer {
             }
         }
 
-        Box::new(token_kinds
-            .iter()
-            .filter_map(|(token_kind, _finish)| -> Option<(TokenKind, &str)> {
-                log!("{} {:?}", "Recognizing".green(), token_kind);
-                match token_kind {
-                    TokenKind::STOP => {
-                        if c.position() >= i.len() {
-                            Some((TokenKind::STOP, ""))
-                        } else {
-                            None
-                        }
-                    }
-                    TokenKind::OBrace => str_recognize(i, c, "{").map(|value| (TokenKind::OBrace, value)),
-                    TokenKind::CBrace => str_recognize(i, c, "}").map(|value| (TokenKind::CBrace, value)),
-                    TokenKind::ComponentKW => {
-                        str_recognize(i, c, "component").map(|value| (TokenKind::ComponentKW, value))
-                    }
-                    TokenKind::ConfigurationKW => str_recognize(i, c, "configuration")
-                        .map(|value| (TokenKind::ConfigurationKW, value)),
-                    TokenKind::CodeKW => {
-                        str_recognize(i, c, "CODE").map(|value| (TokenKind::CodeKW, value))
-                    }
-                    TokenKind::EndCodeKW => {
-                        str_recognize(i, c, "ENDCODE").map(|value| (TokenKind::EndCodeKW, value))
-                    }
-                    TokenKind::TillEndCodeKW => {
-                        consume_until(i, c, &["ENDCODE"]).map(|value| (TokenKind::TillEndCodeKW, value))
-                    }
-                    TokenKind::ModelKW => {
-                        str_recognize(i, c, "model").map(|value| (TokenKind::ModelKW, value))
-                    }
-                    TokenKind::LibraryKW => {
-                        str_recognize(i, c, "library").map(|value| (TokenKind::LibraryKW, value))
-                    }
-                    TokenKind::ID => RE_ID
-                        .find(&i[c.position()..])
-                        .map(|m| (TokenKind::ID, m.as_str())),
-                    TokenKind::Name => RE_NAME
-                        .find(&i[c.position()..])
-                        .map(|m| (TokenKind::Name, m.as_str())),
-                    TokenKind::CommentLine => {
-                        let mut got_true = false;
-                        if i[c.position()..].starts_with("//") {
-                            let skipped_len: usize = i[c.position()..]
-                                .chars()
-                                .take_while(|x| {
-                                    // Need to get the whole line including the newline.
-                                    if got_true {
-                                        return false;
-                                    } else if *x == '\n' {
-                                        got_true = true;
-                                    }
-                                    true
-                                })
-                                .map(|c| c.len_utf8())
-                                .sum();
-                            Some((
-                                TokenKind::CommentLine,
-                                &i[c.position()..(c.position() + skipped_len)],
-                            ))
-                        } else {
-                            None
-                        }
-                    }
-                    TokenKind::CommentName => RE_ID
-                        .find(&i[c.position()..])
-                        .and_then(|name| {
-                            let name = name.as_str();
-                            if name == "START" {
-                                None
+        Box::new(
+            token_kinds
+                .iter()
+                .filter_map(|(token_kind, _finish)| -> Option<(TokenKind, &str)> {
+                    log!("{} {:?}", "Recognizing".green(), token_kind);
+                    match token_kind {
+                        TokenKind::STOP => {
+                            if c.position() >= i.len() {
+                                Some((TokenKind::STOP, ""))
                             } else {
-                                Some(name)
+                                None
                             }
-                        })
-                        .map(|m| (TokenKind::CommentName, m)),
-                    TokenKind::OComment => {
-                        str_recognize(i, c, "/*").map(|value| (TokenKind::OComment, value))
+                        }
+                        TokenKind::OBrace => {
+                            str_recognize(i, c, "{").map(|value| (TokenKind::OBrace, value))
+                        }
+                        TokenKind::CBrace => {
+                            str_recognize(i, c, "}").map(|value| (TokenKind::CBrace, value))
+                        }
+                        TokenKind::ComponentKW => str_recognize(i, c, "component")
+                            .map(|value| (TokenKind::ComponentKW, value)),
+                        TokenKind::ConfigurationKW => str_recognize(i, c, "configuration")
+                            .map(|value| (TokenKind::ConfigurationKW, value)),
+                        TokenKind::CodeKW => {
+                            str_recognize(i, c, "CODE").map(|value| (TokenKind::CodeKW, value))
+                        }
+                        TokenKind::EndCodeKW => str_recognize(i, c, "ENDCODE")
+                            .map(|value| (TokenKind::EndCodeKW, value)),
+                        TokenKind::TillEndCodeKW => consume_until(i, c, &["ENDCODE"])
+                            .map(|value| (TokenKind::TillEndCodeKW, value)),
+                        TokenKind::ModelKW => {
+                            str_recognize(i, c, "model").map(|value| (TokenKind::ModelKW, value))
+                        }
+                        TokenKind::LibraryKW => str_recognize(i, c, "library")
+                            .map(|value| (TokenKind::LibraryKW, value)),
+                        TokenKind::ID => RE_ID
+                            .find(&i[c.position()..])
+                            .map(|m| (TokenKind::ID, m.as_str())),
+                        TokenKind::Name => RE_NAME
+                            .find(&i[c.position()..])
+                            .map(|m| (TokenKind::Name, m.as_str())),
+                        TokenKind::CommentLine => {
+                            let mut got_true = false;
+                            if i[c.position()..].starts_with("//") {
+                                let skipped_len: usize = i[c.position()..]
+                                    .chars()
+                                    .take_while(|x| {
+                                        // Need to get the whole line including the newline.
+                                        if got_true {
+                                            return false;
+                                        } else if *x == '\n' {
+                                            got_true = true;
+                                        }
+                                        true
+                                    })
+                                    .map(|c| c.len_utf8())
+                                    .sum();
+                                Some((
+                                    TokenKind::CommentLine,
+                                    &i[c.position()..(c.position() + skipped_len)],
+                                ))
+                            } else {
+                                None
+                            }
+                        }
+                        TokenKind::CommentName => RE_ID
+                            .find(&i[c.position()..])
+                            .and_then(|name| {
+                                let name = name.as_str();
+                                if name == "START" {
+                                    None
+                                } else {
+                                    Some(name)
+                                }
+                            })
+                            .map(|m| (TokenKind::CommentName, m)),
+                        TokenKind::OComment => {
+                            str_recognize(i, c, "/*").map(|value| (TokenKind::OComment, value))
+                        }
+                        TokenKind::CComment => {
+                            str_recognize(i, c, "*/").map(|value| (TokenKind::CComment, value))
+                        }
+                        TokenKind::NotComment => consume_until(i, c, &["//", "/*", "*/"])
+                            .map(|value| (TokenKind::NotComment, value)),
+                        TokenKind::ModelProperty => RE_MODEL_PROPERTY
+                            .find(&i[c.position()..])
+                            .map(|m| (TokenKind::ModelProperty, m.as_str())),
+                        TokenKind::ConfigurationProperty => RE_CONFIGURATION_PROPERTY
+                            .find(&i[c.position()..])
+                            .map(|m| (TokenKind::ConfigurationProperty, m.as_str())),
+                        TokenKind::WS => RE_WS
+                            .find(&i[c.position()..])
+                            .map(|m| (TokenKind::WS, m.as_str())),
+                        TokenKind::Anything => consume_until(
+                            i,
+                            c,
+                            &[
+                                "//",
+                                "/*",
+                                "{",
+                                "}",
+                                r#"""#,
+                                r#"'"#,
+                                "component",
+                                //"model",
+                                "library",
+                                //"configuration",
+                                "CODE",
+                                "ENDCODE",
+                                "comment",
+                                "START",
+                                "ENDCOMMENT",
+                            ],
+                        )
+                        .map(|value| (TokenKind::Anything, value)),
+                        TokenKind::String => RE_STRING
+                            .find(&i[c.position()..])
+                            .map(|m| (TokenKind::String, m.as_str())),
+                        TokenKind::CommentKW => str_recognize(i, c, "comment")
+                            .map(|value| (TokenKind::CommentKW, value)),
+                        TokenKind::StartCommentKW => str_recognize(i, c, "START")
+                            .map(|value| (TokenKind::StartCommentKW, value)),
+                        TokenKind::EndCommentKW => str_recognize(i, c, "ENDCOMMENT")
+                            .map(|value| (TokenKind::EndCommentKW, value)),
+                        TokenKind::TillEndCommentKW => consume_until(i, c, &["ENDCOMMENT"])
+                            .map(|value| (TokenKind::TillEndCommentKW, value)),
                     }
-                    TokenKind::CComment => {
-                        str_recognize(i, c, "*/").map(|value| (TokenKind::CComment, value))
+                })
+                .map(|(kind, value)| {
+                    log!("    --- {}", "Match!".bold().green());
+                    Token {
+                        kind,
+                        value,
+                        location: Location {
+                            start: Position::Position(c.position()),
+                            end: Some(Position::Position(c.position() + value.len())),
+                        },
                     }
-                    TokenKind::NotComment => consume_until(i, c, &["//", "/*", "*/"])
-                        .map(|value| (TokenKind::NotComment, value)),
-                    TokenKind::ModelProperty => RE_MODEL_PROPERTY
-                        .find(&i[c.position()..])
-                        .map(|m| (TokenKind::ModelProperty, m.as_str())),
-                    TokenKind::ConfigurationProperty => RE_CONFIGURATION_PROPERTY
-                        .find(&i[c.position()..])
-                        .map(|m| (TokenKind::ConfigurationProperty, m.as_str())),
-                    TokenKind::WS => RE_WS
-                        .find(&i[c.position()..])
-                        .map(|m| (TokenKind::WS, m.as_str())),
-                    TokenKind::Anything => consume_until(i, c, &[
-                        "//",
-                        "/*",
-                        "{",
-                        "}",
-                        r#"""#,
-                        r#"'"#,
-                        "component",
-                        //"model",
-                        "library",
-                        //"configuration",
-                        "CODE",
-                        "ENDCODE",
-                        "comment",
-                        "START",
-                        "ENDCOMMENT",
-                    ])
-                    .map(|value| (TokenKind::Anything, value)),
-                    TokenKind::String => RE_STRING
-                        .find(&i[c.position()..])
-                        .map(|m| (TokenKind::String, m.as_str())),
-                    TokenKind::CommentKW => {
-                        str_recognize(i, c, "comment").map(|value| (TokenKind::CommentKW, value))
-                    }
-                    TokenKind::StartCommentKW => {
-                        str_recognize(i, c, "START").map(|value| (TokenKind::StartCommentKW, value))
-                    }
-                    TokenKind::EndCommentKW => {
-                        str_recognize(i, c, "ENDCOMMENT").map(|value| (TokenKind::EndCommentKW, value))
-                    }
-                    TokenKind::TillEndCommentKW => consume_until(i, c, &["ENDCOMMENT"])
-                        .map(|value| (TokenKind::TillEndCommentKW, value)),
-                }
-            })
-            .map(|(kind, value)| {
-                log!("    --- {}", "Match!".bold().green());
-                Token {
-                    kind,
-                    value,
-                    location: Location {
-                        start: Position::Position(c.position()),
-                        end: Some(Position::Position(c.position() + value.len())),
-                    },
-                }
-            }).collect::<Vec<_>>().into_iter())
+                })
+                .collect::<Vec<_>>()
+                .into_iter(),
+        )
     }
 }
